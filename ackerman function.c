aefee2e8e31376a2132bfdf43a7a047db3dca1f7
Kamrul Hasan <mdkamrulhasan989@gmail.com>
2016-08-30 23:57:11 +0600

    Add files via upload

---
+++ ackerman function.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int ackermann(int m,int n)
+{
+    int c;
+    if(m==0)
+        return n+1;
+    if(m!=0 &&n==0)
+        c=ackermann(m-1,1);
+    if(m!=0 && n!=0)
+        c=ackermann(m-1,ackermann(m,n-1));
+    return c;
+}
+int main()
+{
+    int m,n,result;
+    scanf("%d %d",&m,&n);
+    result=ackermann(m,n);
+    printf("%d\n",result);
+    return 0;
+}
